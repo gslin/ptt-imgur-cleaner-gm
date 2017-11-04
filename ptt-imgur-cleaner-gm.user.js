254d7b3ad5ca83819785024b0a88802cbc2ce972
Gea-Suan Lin <gslin@gslin.org>
2017-11-04 12:25:15 +0800

    Greasemonkey Skeleton.

---
+++ ptt-imgur-cleaner-gm.user.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name          Ptt Imgur Cleaner
+// @namespace     https://blog.gslin.org/ptt-imgur-gm
+// @description   Replace smaller version Imgur image to large one.
+// @include       https://www.ptt.cc/*
+// @run-at        document-idle
+// @version       20171104.0
+// ==/UserScript==
