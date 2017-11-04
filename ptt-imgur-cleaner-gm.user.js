efad54f6fbf3dc4a2645ee9445e568f6fa253615
Gea-Suan Lin <gslin@gslin.org>
2017-11-04 12:26:08 +0800

    Implement actual function.

--- ptt-imgur-cleaner-gm.user.js
+++ ptt-imgur-cleaner-gm.user.js
@@ -8,4 +8,16 @@
 // ==/UserScript==
 
 (function(){
+    setInterval(function(){
+        var targets = document.querySelectorAll('.richcontent iframe.imgur-embed-iframe-pub');
+        targets.forEach(function(el){
+            var id = el.getAttribute('id').substring(23);
+
+            // Always using .png
+            var img = document.createElement('img');
+            img.setAttribute('src', '//i.imgur.com/' + id + '.png');
+
+            el.parentNode.replaceChild(img, el);
+        });
+    }, 500);
 })();
