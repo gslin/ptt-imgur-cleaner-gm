2b459e6bebd020120a9c050023af331d66ba3d51
Gea-Suan Lin <gslin@gslin.org>
2019-05-04 05:57:03 +0800

    Refactor.

--- ptt-imgur-cleaner-gm.user.js
+++ ptt-imgur-cleaner-gm.user.js
@@ -4,33 +4,33 @@
 // @description   Replace smaller version Imgur image to large one.
 // @include       https://www.ptt.cc/bbs/*
 // @run-at        document-idle
-// @version       20171114.3
+// @version       0.20190504.0
+// @grant         none
+// @run-at        document-start
 // @license       MIT
 // ==/UserScript==
 
 (function(){
-    var meta = document.createElement('meta');
-    meta.setAttribute('name', 'Referrer');
-    meta.setAttribute('content', 'same-origin');
+    let head = document.getElementsByTagName('head')[0];
 
-    var head = document.getElementsByTagName('head')[0];
+    let meta = document.createElement('meta');
+    meta.setAttribute('content', 'same-origin');
+    meta.setAttribute('name', 'Referrer');
     head.appendChild(meta);
 
-    setInterval(function(){
-        var targets = document.querySelectorAll('.richcontent iframe.imgur-embed-iframe-pub');
-        targets.forEach(function(el){
-            var id = el.getAttribute('id').substring(23);
-
-            // Always using .png
-            var img = document.createElement('img');
-            img.setAttribute('src', '//i.imgur.com/' + id + '.png');
-
-            el.parentNode.replaceChild(img, el);
-        });
-    }, 500);
-
-    var css = document.createElement("style");
-    css.type = "text/css";
+    let css = document.createElement('style');
+    css.type = 'text/css';
     css.innerHTML = ".richcontent { max-width: 100%; }\nimg { max-height: 100%; }";
-    document.body.appendChild(css);
+    head.appendChild(css);
+
+    let targets = document.querySelectorAll('.richcontent iframe.imgur-embed-iframe-pub');
+    targets.forEach(el => {
+        var id = el.getAttribute('id').substring(23);
+
+        // Always using .png
+        var img = document.createElement('img');
+        img.setAttribute('src', '//i.imgur.com/' + id + '.png');
+
+        el.parentNode.replaceChild(img, el);
+    });
 })();
